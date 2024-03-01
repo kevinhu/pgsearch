e020558f45f53780c516a8e6c99f8cfb589e4302
Kevin Hu <kevinhuwest@gmail.com>
2024-03-01 12:20:51 -0800

    build from scratch

---
+++ 01_bootstrap.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# This script is executed at the initialization of the ParadeDB container
+# to configure it with required extensions and Postgres settings
+
+# Exit on subcommand errors
+set -Eeuo pipefail
+
+# If no user is set, the default user will be the `postgres` superuser, so we
+# set the superuser password to default to the user password in that case
+SUPERUSER_PASSWORD=${POSTGRESQL_POSTGRES_PASSWORD:-$POSTGRESQL_PASSWORD}
+
+echo "ParadeDB bootstrap started..."
+echo "Configuring PostgreSQL search path..."
+
+# Add the `paradedb` schema to the user database, and default to public (by listing it first)
+PGPASSWORD=$POSTGRESQL_PASSWORD psql -U "$POSTGRESQL_USERNAME" -d "$POSTGRESQL_DATABASE" -c "ALTER DATABASE $POSTGRESQL_DATABASE SET search_path TO public,paradedb;"
+
+# Add the `paradedb` schema to the template1 database, to have it inherited by all new databases
+# created post-initialization, and default to public (by listing it first)
+PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d template1 -c "ALTER DATABASE template1 SET search_path TO public,paradedb;"
+
+echo "Configuring PostgreSQL permissions..."
+
+# Grant pg_read_all_settings role to the user (necessary for pg_analytics and general database introspection)
+PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d "$POSTGRESQL_DATABASE" -c "GRANT pg_read_all_settings TO \"$POSTGRESQL_USERNAME\";"
+
+echo "Installing PostgreSQL extensions..."
+
+# This setting is required by pg_cron to CREATE EXTENSION properly. It can only be installed in one database, so we install it in
+# the user database. Creating the `pg_cron` extension requires a restart of the PostgreSQL server, so we can't do it here. A restart
+# is already part of the launch process of the Bitnami PostgreSQL container post this point, so the extension can be created by the
+# user after the container has been launched.
+#
+# For simplicity, and because we don't expect most users to use pg_cron, we don't force a restart here and we don't pre-create the
+# extension, leaving it to the user to do it if they want to use it.
+echo "cron.database_name = '$POSTGRESQL_DATABASE'" >>"/opt/bitnami/postgresql/conf/postgresql.conf"
+
+# Pre-install all required PostgreSQL extensions to the user database via the `postgres` superuser
+PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d "$POSTGRESQL_DATABASE" -c "CREATE EXTENSION IF NOT EXISTS pg_bm25 CASCADE;"
+# PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d "$POSTGRESQL_DATABASE" -c "CREATE EXTENSION IF NOT EXISTS pg_analytics CASCADE;"
+# PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d "$POSTGRESQL_DATABASE" -c "CREATE EXTENSION IF NOT EXISTS svector CASCADE;"
+# PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d "$POSTGRESQL_DATABASE" -c "CREATE EXTENSION IF NOT EXISTS vector CASCADE;"
+PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d "$POSTGRESQL_DATABASE" -c "CREATE EXTENSION IF NOT EXISTS vectors CASCADE;"
+
+# Pre-install all required PostgreSQL extensions to the template1 database, to have them inherited by all new
+# databases created post-initialization, via the `postgres` user
+PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d template1 -c "CREATE EXTENSION IF NOT EXISTS pg_bm25 CASCADE;"
+# PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d template1 -c "CREATE EXTENSION IF NOT EXISTS pg_analytics CASCADE;"
+# PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d template1 -c "CREATE EXTENSION IF NOT EXISTS svector CASCADE;"
+# PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d template1 -c "CREATE EXTENSION IF NOT EXISTS vector CASCADE;"
+PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d template1 -c "CREATE EXTENSION IF NOT EXISTS vectors CASCADE;"
+
+# echo "Sending anonymous deployment telemetry (to turn off, unset PARADEDB_TELEMETRY)..."
+
+# We collect basic, anonymous telemetry to help us understand how many people are using
+# the project. We only do this if PARADEDB_TELEMETRY is set to "true"
+# if [[ ${PARADEDB_TELEMETRY:-} == "true" ]]; then
+#   # For privacy reasons, we generate an anonymous UUID for each new deployment
+#   UUID_FILE="/bitnami/postgresql/data/paradedb_uuid"
+#   if [ ! -f "$UUID_FILE" ]; then
+#     uuidgen > "$UUID_FILE"
+#   fi
+#   DISTINCT_ID=$(cat "$UUID_FILE")
+
+#   # Send the deployment event to PostHog
+#   curl -s -L --header "Content-Type: application/json" -d '{
+#     "api_key": "'"$POSTHOG_API_KEY"'",
+#     "event": "ParadeDB Deployment",
+#     "distinct_id": "'"$DISTINCT_ID"'",
+#     "properties": {
+#       "commit_sha": "'"${COMMIT_SHA:-}"'"
+#     }
+#   }' "$POSTHOG_HOST/capture/"
+
+#   # Mark telemetry as handled so we don't try to send it again when
+#   # initializing our PostgreSQL extensions. We use a file for IPC
+#   # between this script and our PostgreSQL extensions
+#   echo "true" > /tmp/telemetry
+# fi
+
+echo "ParadeDB bootstrap completed!"
--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,26 @@
-FROM paradedb/paradedb:0.5.7
+ARG PG_VERSION_MAJOR=16
+
+###############################################
+# First Stage: Builder
+###############################################
+
+# We build the extensions on the official PostgreSQL image, since the Bitnami
+# image does not have root access and necessary build tools
+FROM postgres:${PG_VERSION_MAJOR}-bullseye as builder
 
 ARG PG_VERSION_MAJOR=16
+
 ARG RUST_VERSION=1.76.0
 ARG PGRX_VERSION=0.11.2
 ARG PGVECTO_RS_VERSION=0.2.0
+ARG PARADEDB_VERSION=0.5.7
 
 # Declare buildtime environment variables
 ENV PG_VERSION_MAJOR=${PG_VERSION_MAJOR} \
     RUST_VERSION=${RUST_VERSION} \
+    PGVECTO_RS_VERSION=${PGVECTO_RS_VERSION} \
     PGRX_VERSION=${PGRX_VERSION} \
-    PGVECTO_RS_VERSION=${PGVECTO_RS_VERSION}
-
-USER root
+    PARADEDB_VERSION=${PARADEDB_VERSION}
 
 RUN apt-get update && apt-get install -y --no-install-recommends \
     software-properties-common \
@@ -22,7 +31,7 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
     git \
     make \
     gcc \
-    clang \
+    clang-16 \
     pkg-config \
     postgresql-server-dev-all \
     && rm -rf /var/lib/apt/lists/*
@@ -31,12 +40,56 @@ RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y \
     && /root/.cargo/bin/rustup default "${RUST_VERSION}"
 
 ENV PATH="/root/.cargo/bin:$PATH" \
-    PGX_HOME=/usr/lib/postgresql/${PG_VERSION_MAJOR}
+    PGX_HOME=/usr/lib/postgresql/${PG_VERSION_MAJOR} \
+    PG_VERSION_MAJOR=${PG_VERSION_MAJOR}
+
+RUN cargo install --locked cargo-pgrx --version "${PGRX_VERSION}"
+RUN cargo pgrx init --pg${PG_VERSION_MAJOR}=/usr/lib/postgresql/${PG_VERSION_MAJOR}/bin/pg_config
+
+######################
+# pg_bm25
+######################
+FROM builder as builder-pg_bm25
+
+RUN git clone --branch v${PARADEDB_VERSION} https://github.com/paradedb/paradedb
+RUN cd paradedb/pg_bm25 && cargo pgrx package --features icu --pg-config "/usr/lib/postgresql/${PG_VERSION_MAJOR}/bin/pg_config"
+
+######################
+# pgvecto.rs
+######################
+FROM builder as builder-pgvecto_rs
 
 RUN git clone --branch v${PGVECTO_RS_VERSION} https://github.com/tensorchord/pgvecto.rs
+RUN cd pgvecto.rs && cargo pgrx package --pg-config "/usr/lib/postgresql/${PG_VERSION_MAJOR}/bin/pg_config"
 
-WORKDIR /pgvecto.rs
+###############################################
+# Second Stage: PostgreSQL
+###############################################
 
-RUN cargo install cargo-pgrx@$(grep 'pgrx = { version' Cargo.toml | cut -d '"' -f 2)
-RUN cargo pgrx init "--pg${PG_VERSION_MAJOR}=/usr/lib/postgresql/${PG_VERSION_MAJOR}/bin/pg_config"
-RUN cargo pgrx install --release
+FROM bitnami/postgresql:${PG_VERSION_MAJOR}-debian-11 as pgsearch
+
+ARG PG_VERSION_MAJOR=16
+
+ENV PG_VERSION_MAJOR=${PG_VERSION_MAJOR} \
+    POSTHOG_API_KEY='' \
+    POSTHOG_HOST='' \
+    PARADEDB_TELEMETRY=false
+
+COPY --from=builder-pgvecto_rs /pgvecto.rs/target/release/vectors-pg${PG_VERSION_MAJOR}/usr/lib/postgresql/${PG_VERSION_MAJOR}/lib/* /opt/bitnami/postgresql/lib/
+COPY --from=builder-pgvecto_rs /pgvecto.rs/target/release/vectors-pg${PG_VERSION_MAJOR}/usr/share/postgresql/${PG_VERSION_MAJOR}/extension/* /opt/bitnami/postgresql/share/extension/
+
+COPY --from=builder-pg_bm25 /paradedb/pg_bm25/target/release/pg_bm25-pg${PG_VERSION_MAJOR}/usr/lib/postgresql/${PG_VERSION_MAJOR}/lib/* /opt/bitnami/postgresql/lib/
+COPY --from=builder-pg_bm25 /paradedb/pg_bm25/target/release/pg_bm25-pg${PG_VERSION_MAJOR}/usr/share/postgresql/${PG_VERSION_MAJOR}/extension/* /opt/bitnami/postgresql/share/extension/
+
+# Install runtime dependencies (requires switching to root temporarily)
+USER root
+RUN install_packages curl uuid-runtime libpq5
+USER 1001
+
+# Copy ParadeDB bootstrap script to install extensions, configure postgresql.conf, etc.
+# COPY ./docker/01_bootstrap.sh /docker-entrypoint-initdb.d/
+COPY ./01_bootstrap.sh /docker-entrypoint-initdb.d/
+
+# Configure shared_preload_libraries
+# Note: pgaudit is needed here as it comes pre-packaged in the Bitnami image
+ENV POSTGRESQL_SHARED_PRELOAD_LIBRARIES="pgaudit,pg_bm25,vectors"
